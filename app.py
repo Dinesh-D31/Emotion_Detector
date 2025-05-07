950e6d2442b2774c93e84f67d3143145ef7e23c4
Dinesh D <132872540+Dinesh-D31@users.noreply.github.com>
2025-05-07 11:43:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 # Load Custom CSS
-with open("C:/Users/Asus/Documents/projects/style.css") as f:
+with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Load Model, Vectorizer and Label Binarizer
